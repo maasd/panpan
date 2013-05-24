f9edabd3f771d522c6a0a5b0b9cd997967347f5f
Maas Dianto <maas.dianto@gmail.com>
2013-05-24 09:52:20 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papan::Application.config.secret_token = '31523f42179089696bdf5f7fa4a21952fafa0d1574205e4c1a4654392001fe77b9c333297c0a8da2bed43dbe6bc37bd8611e7eb61877ebf25b22295048adcba3'
